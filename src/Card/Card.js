ab9774bf3986dbd8441c184744090f97e8cf0b03
Artsiom Stsepaniuk <astsepaniuk@gomel.iba.by>
2020-01-27 09:59:30 +0300

    Task 3. Add Edit action

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,19 +1,89 @@
-//import React from 'react';
 import React, { Component } from 'react';
 import './Card.css';
+import { MdEdit, MdSave, MdCancel } from 'react-icons/md';
 
 class Card extends Component {
   state = {
-    cbChecked: false
+    cardHeaderText: 'Tab number One',
+    cardBodyText: 'I expect some text here...',
+    cardHeaderTemp: '',
+    cardBodyTemp: '',
+    cbChecked: false,
+    cardEdit: false
   };
 
   checkBoxHandler = () => {
     this.setState({cbChecked: !this.state.cbChecked});
   };
 
+  cardEditHandler = () => {
+    this.setState({
+        cardEdit: !this.state.cardEdit,
+        cardHeaderTemp: this.state.cardHeaderText,
+        cardBodyTemp: this.state.cardBodyText
+    });
+  };
+
+  cardSaveHandler = () => {
+    this.setState({cardHeaderText: this.state.cardHeaderTemp});
+    this.setState({cardBodyText: this.state.cardBodyTemp});
+    this.setState({cardEdit: !this.state.cardEdit});
+  };
+
+  cardCancelHandler = () => {
+    this.setState({cardEdit: !this.state.cardEdit});
+  };
+
+  cardChangeHeader = (event) => {      
+      this.setState({cardHeaderTemp:  event.target.value});
+  };
+
+  cardChangeBody = (event) => {      
+    this.setState({cardBodyTemp:  event.target.value});
+  };
+    
   render() {
     let textStyle = this.state.cbChecked ? "redText" : "blackText";
-        
+    let cardActions = null;
+    let cardHeader = null;
+    let cardBody = null;    
+  
+    if (this.state.cardEdit) {    
+      cardActions = (
+        <span>
+          <MdSave className="cardSave" onClick={this.cardSaveHandler}/>
+          <MdCancel className="cardCancel" onClick={this.cardCancelHandler}/>          
+        </span>
+      );
+      cardHeader = (
+        <span>
+          <input type='text' value={this.state.cardHeaderTemp} onChange={this.cardChangeHeader}/>
+        </span>
+      );
+      cardBody = (
+        <span>
+          <input type='text' value={this.state.cardBodyTemp} onChange={this.cardChangeBody}/>
+        </span>
+      );      
+    } else {
+      cardHeader = (
+        <span>
+          {this.state.cardHeaderText}
+        </span>
+      );
+      cardBody = (
+        <span>
+          {this.state.cardBodyText}
+        </span>
+      );
+      cardActions = (
+        <span>
+          <MdEdit className="cardEdit" onClick={this.cardEditHandler} />
+          <input className="cardCheckmark" type="checkbox" onChange={this.checkBoxHandler} />          
+        </span>
+      );
+    };
+
     return (
       <div className="card">
         <table className="cardTable">
@@ -23,9 +93,11 @@ class Card extends Component {
                 <table className="cardHeader">
                   <tbody>
                     <tr>
-                      <td>Tab number One</td>
+                      <td>
+                        {cardHeader}
+                      </td>
                       <td className="cardCmPosition">
-                        <input className="cardCheckmark" type="checkbox" onChange={this.checkBoxHandler} />
+                        {cardActions}
                       </td>
                     </tr>
                   </tbody>
@@ -35,7 +107,9 @@ class Card extends Component {
             </tr>
             <tr>
               <td>
-                <p className={textStyle}>I expect some text here...</p>
+                <p className={textStyle}>
+                  {cardBody}
+                </p>
               </td>
             </tr>
           </tbody>
